a0357f39fb61ce0050ae04a569ce3ade2ebe17b1
abspen1 <abspencer2097@gmail.com>
2020-09-17 17:12:39 -0700

    init

---
+++ helper.py
@@ -0,0 +1,16 @@
+import os
+import redis
+
+def main():
+    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
+                         password=os.getenv("REDIS_PASS"))
+    # Using a hash
+    # client.hset('bday', 'First Last Mon 01', 'email@domain')
+    
+
+    
+    
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,55 @@
+import datetime
+import json
+import os
+import random
+import redis
+import requests
+import schedule
+import time
+
+BASE_URL = "https://guldentech.com/austinapi/bdayemail"
+
+JOKE_URL = "https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/"
+
+
+def get_joke():
+    types = ("general", "knock-knock")
+    index = random.randint(0,1)
+    url = JOKE_URL + types[index]
+    r = requests.get(url)
+    return json.loads(r.content)
+
+
+def get_formatted_date():
+    # Get date in format Mon 01
+    date = time.ctime()[4:10]
+    return date
+
+
+def send_email(name, email):
+    joke = get_joke()
+    for item in joke:
+        setup = item["setup"]
+        punchline = item["punchline"]
+    dictionary = {"Name": name, "Email": email, "JokeSetup": setup, "JokePunchLine": punchline}
+    dictionary = json.dumps(dictionary, indent=4)
+    x = requests.post(BASE_URL, data=dictionary)
+
+    print(x.text)
+
+
+def main():
+    date = get_formatted_date()
+    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
+                         password=os.getenv("REDIS_PASS"))
+    birthday_dict = client.hgetall('bday')
+
+    for name_date, email in birthday_dict.items():
+        if date == name_date.decode("utf-8")[-6:]:
+            name = name_date.decode("utf-8")[:-7]
+            email = email.decode("utf-8")
+            send_email(name, email)
+
+    
+if __name__ == "__main__":
+    main()
