0c5cb2a5c4e2638bf8c6e17fcb105206f5ca4e0d
abspen1 <abspencer2097@gmail.com>
2020-09-18 16:00:18 -0700

    add a function to see each birthday

--- helper.py
+++ helper.py
@@ -1,6 +1,13 @@
 import os
 import redis
 
+def get_bdays():
+    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
+                         password=os.getenv("REDIS_PASS"))
+    dictionary = client.hgetall('bday')
+    for item, key in dictionary.items():
+        print(item, key, "\n\n")
+
 def main(name, birthdate, email):
     client = redis.Redis(host="10.10.10.1", port=6379, db=6,
                          password=os.getenv("REDIS_PASS"))
@@ -8,6 +15,8 @@ def main(name, birthdate, email):
     # client.hset('bday', 'First Last Mon 01', 'email@domain')
     concatenate = name + " " + birthdate
     client.hset('bday', concatenate, email)
+    
 
 if __name__ == "__main__":
     main()
+    get_bdays()
