9e1ed223aa5f155ff53393c4c4fd7f578b573dd2
abspen1 <abspencer2097@gmail.com>
2020-09-18 01:28:59 -0700

    add variables so that we can quick add new people

--- helper.py
+++ helper.py
@@ -1,11 +1,13 @@
 import os
 import redis
 
-def main():
+def main(name, birthdate, email):
     client = redis.Redis(host="10.10.10.1", port=6379, db=6,
                          password=os.getenv("REDIS_PASS"))
     # Using a hash
     # client.hset('bday', 'First Last Mon 01', 'email@domain')
+    concatenate = name + " " + birthdate
+    client.hset('bday', concatenate, email)
 
 if __name__ == "__main__":
     main()
