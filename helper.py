ee8b872e47ec5b7e1a878f234ee1474b36478cc6
abspen1 <abspencer2097@gmail.com>
2020-09-28 09:36:27 -0700

    added remove function

--- helper.py
+++ helper.py
@@ -8,6 +8,11 @@ def get_bdays():
     for item, key in dictionary.items():
         print(item, key, "\n\n")
 
+def remove_bdays():
+    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
+                         password=os.getenv("REDIS_PASS"))
+    client.hdel("bday", "First Last Mon 01")
+    
 def main(name, birthdate, email):
     client = redis.Redis(host="10.10.10.1", port=6379, db=6,
                          password=os.getenv("REDIS_PASS"))
@@ -18,5 +23,5 @@ def main(name, birthdate, email):
     
 
 if __name__ == "__main__":
-    main("Test with Go", "Sep 26", "abspencer2097@yahoo.com")
+    remove_bdays()
     get_bdays()
