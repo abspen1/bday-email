a333839b952332dff96d9f4d70de240afdfc9282
abspen1 <abspencer2097@gmail.com>
2020-11-28 15:21:50 -0700

    messing around

--- helper.py
+++ helper.py
@@ -1,21 +1,19 @@
 import os
 import redis
 
+client = redis.Redis(host="10.10.10.1", port=6379, db=6,
+                     password=os.getenv("REDIS_PASS"))
+
 def get_bdays():
-    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
-                         password=os.getenv("REDIS_PASS"))
     dictionary = client.hgetall('bday')
     for item, key in dictionary.items():
         print(item, key, "\n\n")
 
-def remove_bdays():
-    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
-                         password=os.getenv("REDIS_PASS"))
-    client.hdel("bday", "First Last Mon 01")
+def remove_bdays(name, birthdate):
+    concatenate = name + " " + birthdate
+    client.hdel("bday", concatenate)
     
 def main(name, birthdate, email):
-    client = redis.Redis(host="10.10.10.1", port=6379, db=6,
-                         password=os.getenv("REDIS_PASS"))
     # Using a hash
     # client.hset('bday', 'First Last Mon 01', 'email@domain')
     concatenate = name + " " + birthdate
@@ -23,5 +21,5 @@ def main(name, birthdate, email):
     
 
 if __name__ == "__main__":
-    remove_bdays()
-    get_bdays()
+    # remove_bdays()
+    print(client.get('back-end-auth'))
